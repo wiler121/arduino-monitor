c62af52bceadf7f0af647bd503ad9a4d8e56afd6
wiler121 <wiler121@gmail.com>
2021-02-24 18:06:58 +0100

    Initial Project Add

---
+++ src/main/java/mf/arduino/arduinomonitor/ArduinoMonitorApplication.java
@@ -0,0 +1,13 @@
+package mf.arduino.arduinomonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArduinoMonitorApplication.class, args);
+    }
+
+}
