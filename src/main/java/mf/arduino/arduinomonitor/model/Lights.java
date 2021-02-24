6e920d4186dd3f05ce744045dfbbd7805e0822d5
wiler121 <wiler121@gmail.com>
2021-02-24 18:46:25 +0100

    Pojo. Closes #2.

--- src/main/java/mf/arduino/arduinomonitor/model/Lights.java
+++ src/main/java/mf/arduino/arduinomonitor/model/Lights.java
@@ -3,4 +3,11 @@ package mf.arduino.arduinomonitor.model;
 public class Lights {
     int lightLevel;
 
+    public int getLightLevel() {
+        return lightLevel;
+    }
+
+    public void setLightLevel(int lightLevel) {
+        this.lightLevel = lightLevel;
+    }
 }
--- src/main/java/mf/arduino/arduinomonitor/model/Motion.java
+++ src/main/java/mf/arduino/arduinomonitor/model/Motion.java
@@ -2,4 +2,12 @@ package mf.arduino.arduinomonitor.model;
 
 public class Motion {
     int motion;
+
+    public int getMotion() {
+        return motion;
+    }
+
+    public void setMotion(int motion) {
+        this.motion = motion;
+    }
 }
